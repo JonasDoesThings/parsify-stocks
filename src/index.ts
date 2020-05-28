6061d607ce8aa6a9ca44f52f8ea593217cbedb3f
Antoni Kepinski <a@kepinski.me>
2020-05-28 19:43:54 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ interface Options {
 	It receives the expression, which you need to validate and either do something with it or return the original expression.
 */
 export default ({upperCase = false}: Options = {}) => async (expression: string): Promise<string> => {
+	/*
+		Users can't set individual arguments for each plugin in Parsify Desktop.
+		Instead, they can use the global configuration in settings, which uses environmental variables.
+		You may want to remove the following block if you are not going to use this plugin in Parsify Desktop.
+	*/
+	if (process.env.UPPER_CASE) {
+		// Since enviromental variables are just strings, we need to parse them.
+		upperCase = process.env.UPPER_CASE === 'true' && true;
+	}
+
 	// Validate the expression to see if it should be processed by your plugin
 	if (expression === 'hello') {
 		if (upperCase) {
